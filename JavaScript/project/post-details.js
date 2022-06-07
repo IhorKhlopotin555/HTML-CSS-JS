0ba6b96e6cf05699a2237b2ccb41f8b66cbf60f7
IhorKhlopotin555 <hlopotinigor555@gmail.com>
2022-06-07 17:28:03 +0300

    add lesson 1-12 and project

--- JavaScript/project/post-details.js
+++ JavaScript/project/post-details.js
@@ -1,17 +1,19 @@
 let url = new URL(location.href);
-let user = JSON.parse(url.searchParams.get('data'));
-console.log(user);
+let post = JSON.parse(url.searchParams.get('data'));
+console.log(post);
 
-let postContainer = document.getElementsByClassName('post')[0];
-postContainer.innerText = JSON.stringify('post');
 
-let commentsContainer = document.getElementsByClassName('comments')[0];
-fetch(`https://jsonplaceholder.typicode.com/posts/${post.id}/comments`)
-    .then(value => value.json())
-    .then(value => {
-            for (const commentItem of value){
-            commentContainer = document.createElement('li');
-            commentContainer.innerText = commentItem.body;
-            commentsContainer.appendChild(commentContainer)
-            }
-        })
+let postContainer = document.getElementsByClassName('post')[0]
+console.log(postContainer);
+postContainer.innerText = JSON.stringify(post);
+
+// let commentsContainer = document.getElementsByClassName('comments')[0];
+// fetch(`https://jsonplaceholder.typicode.com/posts/${post.id}/comments`)
+//     .then(value => value.json())
+//     .then(value => {
+//             for (const commentItem of value){
+//             commentContainer = document.createElement('li');
+//             commentContainer.innerText = commentItem.body;
+//             commentsContainer.appendChild(commentContainer)
+//             }
+//         })
--- JavaScript/project/user-details.js
+++ JavaScript/project/user-details.js
@@ -1,17 +1,69 @@
 let url = new URL(location.href);
 let user = JSON.parse(url.searchParams.get('data'));
-console.log(user);
 
 let userDetailsContainer = document.getElementsByClassName('user-details')[0];
-let postsContainer = document.getElementsByClassName('posts')[0];
+let postsContainer = document.getElementsByClassName('postContainer')[0];
 let postsDetailsButton = document.getElementsByClassName('postsDetailsButton')[0];
+console.log(typeof postsDetailsButton_c);
 userDetailsContainer.innerText = `${JSON.stringify(user)}`;
-postsButton.onclick = function () {
-    .this.disabled = true;
+for(const elem in user) {
+    if (typeof user[elem] === 'string') {
+        let el = document.createElement('div')
+        el.innerText = `${elem} - ${user[elem]}`
+        userDetailsContainer.appendChild(el)
+    } else {
+        let el = document.createElement('div')
+        el.innerText = `${elem}`
+        userDetailsContainer.appendChild(el)
+        for (const el in user[elem]) {
+            if (typeof user[elem][el] ==='string') {
+                let e = document.createElement('div')
+                e.innerText = `${user[elem][el]}`
+                userDetailsContainer.appendChild(e)
+            }
+            else{
+                let e = document.createElement('div')
+                e.innerText = `${el}`
+                userDetailsContainer.appendChild(e)
+                for(const volodya in user[elem][el]){
+                    let l = document.createElement('div')
+                    l.innerText = `${volodya} - ${user[elem][el][volodya]}`
+                    userDetailsContainer.appendChild(l)
+                }
+            }
+        }
+    }
+}
+
+// postsButton.addEventListener('click', function () {
+//     this.disabled = true;
+//
+//     fetch(`https://jsonplaceholder.typicode.com/users/${user.id}/posts`)
+//         .then(value => value.json())
+//         .then(value => {
+//             console.log(value);
+//             for (const postItem of value){
+//                 let postContainer = document.createElement('div');
+//                 postContainer.classList.add('post');
+//                 postContainer.innerText = postItem.title;
+//                 let postDetailsButton = document.createElement('button');
+//                 postDetailsButton.innerText = 'post details'
+//                 postDetailsButton.onclick = function () {
+//                     location.href = `./post-details.html?data=${JSON.stringify(postItem)}`
+//                 }
+//
+//                 postContainer.appendChild(postDetailsButton);
+//                 postsContainer.append(postContainer);
+//             }
+//         })
+//  })
+postsDetailsButton .onclick = function () {
+    this.disabled = true;
 
     fetch(`https://jsonplaceholder.typicode.com/users/${user.id}/posts`)
         .then(value => value.json())
         .then(value => {
+            console.log(value);
             for (const postItem of value){
                 let postContainer = document.createElement('div');
                 postContainer.classList.add('post');
